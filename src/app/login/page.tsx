57c922e48cfe20692cc1df4dcea5e135df2f5029
Roxmatullox <roxmatulloxprgrmr@gmail.com>
2023-11-12 01:14:39 +0500

    rox

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,11 @@ import Login from "@/components/Login/login"
 const LoginPage = () => {
   return (
     <main>
+      <h1 style={{
+        textAlign:"center",
+        color:"white",
+        marginBottom:"30px"
+      }}>Login</h1>
       <Login />
     </main>
   )
